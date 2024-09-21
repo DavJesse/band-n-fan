b4c338ff1bb9c25146bd4ffc2c143cbd23bc6f2a
davodhiambo <jesseomolo@yahoo.com>
2024-09-21 20:53:55 +0300

    refactor: implement error handling in home.go by calling error handlers.

--- internal/handlers/error_handler.go
+++ internal/handlers/error_handler.go
@@ -29,19 +29,19 @@ func badRequestHandler(w http.ResponseWriter) {
 func internalServerErrorHandler(w http.ResponseWriter) {
 	tmpl, err = template.ParseFiles("web/templates/error.html")
 
-		// Use writer to render error if error page unavailable
-		if err != nil {
-			http.Error(w, "Could not load template, error page unavailable", http.StatusInternalServerError)
-			return
-		}
+	// Use writer to render error if error page unavailable
+	if err != nil {
+		http.Error(w, "Could not load template, error page unavailable", http.StatusInternalServerError)
+		return
+	}
 
-		hitch.StatusCode = http.StatusInternalServerError
-		hitch.Problem = "Internal Server Error!"
+	hitch.StatusCode = http.StatusInternalServerError
+	hitch.Problem = "Internal Server Error!"
 
-		// Render template in html
-		err = tmpl.Execute(w, hitch)
-		if err != nil {
-			http.Error(w, "Could not execute error template, error page unavailable", http.StatusInternalServerError)
-			log.Println("Error executing template: ", err)
-		}
+	// Render template in html
+	err = tmpl.Execute(w, hitch)
+	if err != nil {
+		http.Error(w, "Could not execute error template, error page unavailable", http.StatusInternalServerError)
+		log.Println("Error executing template: ", err)
+	}
 }
--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -7,45 +7,39 @@ import (
 	"net/http"
 )
 
-var data, err = api.LoadData() // Load data from API
-var tmpl *template.Template
-
 type Issue struct {
 	StatusCode int
 	Problem    string
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	// Restrict access to home page
-	if r.Method != "GET" {
-		tmpl, err = template.ParseFiles("web/templates/error.html")
-		Issue.StatusCode = http.StatusBadRequest
+var data, err = api.LoadData() // Load data from API
+var tmpl *template.Template
+var hitch Issue
 
-		tmpl.Execute()
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// Restrict access to home page, execute error template
+	if r.Method != "GET" {
+		badRequestHandler(w)
 		return
 	}
 
 	// handle errors from API call
 	if err != nil {
-		http.Error(w, "Could not load API data: ", http.StatusInternalServerError)
-		log.Println("Error loading API data: ", err)
+		internalServerErrorHandler(w)
+		return
 	}
 
 	// Create Html template from home.html, handle errors if necessary
 	tmpl, err = template.ParseFiles("web/templates/home.html")
-
 	if err != nil {
-		http.Error(w, "Could not load home template", http.StatusInternalServerError)
-		log.Println("Error parsing template: ", err)
+		internalServerErrorHandler(w)
 		return
 	}
 
 	// Safely execute tmpl, handle errors if necessary
 	err = tmpl.Execute(w, data)
 	if err != nil {
-		http.Error(w, "Could not execute home template", http.StatusInternalServerError)
-		log.Println("Error executing template: ", err)
+		internalServerErrorHandler(w)
 		return
 	}
 }
