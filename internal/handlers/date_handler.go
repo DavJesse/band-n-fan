a72ddfc8245e8a1d37b9ff51808fa202ada72038
davodhiambo <jesseomolo@yahoo.com>
2024-09-21 17:38:26 +0300

    feat: add loop system to populate BandName field of data.Dates.Index structs.

--- internal/handlers/date_handler.go
+++ internal/handlers/date_handler.go
@@ -14,6 +14,18 @@ func DateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Populate BandName field of Dates struct
+	for i := range data.Dates.Index {
+		for j := i; j < len(data.Artists); j++ {
+
+			// if data.Dates.Index.Id and data.Artist.Id match, update BandName in dates.Index[i]
+			if data.Artists[j].Id == data.Dates.Index[i].Id {
+				data.Dates.Index[i].BandName = data.Artists[j].Name
+				i++ // Break loop, match found
+			}
+		}
+	}
+
 	err = tmpl.Execute(w, data.Dates)
 	if err != nil {
 		http.Error(w, "Failed to execute template", http.StatusInternalServerError)
