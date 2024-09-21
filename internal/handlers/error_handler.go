3ee1dc47b5530db3a5cc66e5126080be0eebf107
davodhiambo <jesseomolo@yahoo.com>
2024-09-21 21:14:56 +0300

    feat: add notFoundHandler to handers package.

--- internal/handlers/error_handler.go
+++ internal/handlers/error_handler.go
@@ -45,3 +45,23 @@ func internalServerErrorHandler(w http.ResponseWriter) {
 		log.Println("Error executing template: ", err)
 	}
 }
+
+func notFoundHandler(w http.ResponseWriter) {
+	tmpl, err = template.ParseFiles("web/templates/error.html")
+
+	// Use writer to render error if error page unavailable
+	if err != nil {
+		http.Error(w, "Could not load template, error page unavailable", http.StatusInternalServerError)
+		return
+	}
+
+	hitch.StatusCode = http.StatusNotFound
+	hitch.Problem = "Not Found!"
+
+	// Render template in html
+	err = tmpl.Execute(w, hitch)
+	if err != nil {
+		http.Error(w, "Could not execute error template, error page unavailable", http.StatusInternalServerError)
+		log.Println("Error executing template: ", err)
+	}
+}
