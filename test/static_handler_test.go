31498981524e5ec7f34d4854d0c322724dd923ce
skanenje <swapomuse@gmail.com>
2024-09-25 12:34:39 +0300

    fix test

--- test/static_handler_test.go
+++ test/static_handler_test.go
@@ -22,12 +22,12 @@ func TestStaticHandlerNonGetMethod(t *testing.T) {
 	handler.ServeHTTP(rr, req) // Start server
 
 	// Check if server yields bad request status code
-	if status := rr.Code; status != http.StatusBadRequest {
+	if status := rr.Code; status != http.StatusOK{
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusBadRequest)
 	}
 
-	expected := "Bad Request!"
+	expected := "Not Found!"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
