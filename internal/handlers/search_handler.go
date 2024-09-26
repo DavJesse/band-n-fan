2c899965fcb727b2ccda3fb8650c24b362198649
davodhiambo <jesseomolo@yahoo.com>
2024-09-26 06:18:09 +0300

    feat: add SearchArtist function to handlers package.

---
+++ internal/handlers/search_handler.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"groupie-tracker/internal/api"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func SearchHandler(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func SearchArtist(query string) api.Data {
+	var result api.Data
+	query = strings.ToLower(query)
+
+	// Search for matching artist
+	for _, artist := range Data.Artists {
+		// Search by name
+		if strings.Contains(artist.Name, query) {
+			result.Artists = append(result.Artists, artist)
+		}
+		// Search by FirstAlbum Date
+		if strings.Contains(artist.FirstAlbum, query) {
+			result.Artists = append(result.Artists, artist)
+		}
+		// Search by creation date
+		if isDecimal(query) {
+			date, _ := strconv.Atoi(query)
+			if date == artist.CreationDate {
+				result.Artists = append(result.Artists, artist)
+			}
+		}
+
+		// Search by band members
+		for i := range artist.Members {
+			if strings.Contains(artist.Members[i], query) {
+				result.Artists = append(result.Artists, artist)
+				break
+			}
+		}
+	}
+
+	// Search for matching date
+	for _, dateObj := range Data.Dates.Index {
+		for _, date := range dateObj.Dates {
+			if strings.Contains(date, query) {
+				result.Dates.Index = append(result.Dates.Index, dateObj)
+			}
+		}
+	}
+
+	// Search for matching locations
+	for _, locationObj := range Data.Locations.Index {
+		for _, location := range locationObj.Locations {
+			if strings.Contains(location, query) {
+				result.Locations.Index = append(result.Locations.Index, locationObj)
+			}
+		}
+	}
+
+	return result
+}
