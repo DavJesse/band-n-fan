a6639a2404a3fb5f0084b884fb7be1e635750588
davodhiambo <jesseomolo@yahoo.com>
2024-09-29 09:31:51 +0300

    chore : remove search functionality.

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	http.HandleFunc("/dates", handlers.DateHandler)          // Serve date.html
 	http.HandleFunc("/locations", handlers.LocationsHandler) // Serve location.html
 	http.HandleFunc("/relations", handlers.RelationsHandler) // Serve relation.html
-	http.HandleFunc("/results", handlers.SearchHandler)      // Serve search.html
+	//http.HandleFunc("/results", handlers.SearchHandler)      // Serve search.html
 
 	// Start server
 	log.Println("Starting server on port 8080")
